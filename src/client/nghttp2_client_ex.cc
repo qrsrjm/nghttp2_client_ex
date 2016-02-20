490af44603b3504a98a18df5381255ef3e09684d
Reed Morrison <rmorrison@edgecast.com>
2016-02-20 14:36:26 -0800

    Adding pristine version of nghttp2 lib source.

---
+++ src/client/nghttp2_client_ex.cc
@@ -0,0 +1,131 @@
+//: ----------------------------------------------------------------------------
+//: \file:    nghttp2_client_ex.cc
+//: \details: TODO
+//: \author:  Reed P. Morrison
+//: \date:    02/20/2016
+//: ----------------------------------------------------------------------------
+
+//: ----------------------------------------------------------------------------
+//: Includes
+//: ----------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h> // For getopt_long
+#include <stdint.h>
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+#include <inttypes.h>
+#include <string>
+
+//: ----------------------------------------------------------------------------
+//: \details: Print the version.
+//: \return:  TODO
+//: \param:   TODO
+//: ----------------------------------------------------------------------------
+void print_version(FILE* a_stream, int a_exit_code)
+{
+        fprintf(a_stream, "nghttp2 client example.\n");
+        fprintf(a_stream, "               Version: %s\n", "0.0.0");
+        exit(a_exit_code);
+}
+
+//: ----------------------------------------------------------------------------
+//: \details: Print the command line help.
+//: \return:  TODO
+//: \param:   TODO
+//: ----------------------------------------------------------------------------
+void print_usage(FILE* a_stream, int a_exit_code)
+{
+        fprintf(a_stream, "Usage: phurl -u [http[s]://]hostname[:port]/path [options]\n");
+        fprintf(a_stream, "Options are:\n");
+        fprintf(a_stream, "  -h, --help           Display this help and exit.\n");
+        fprintf(a_stream, "  -V, --version        Display the version number and exit.\n");
+        fprintf(a_stream, "  \n");
+        exit(a_exit_code);
+}
+
+//: ----------------------------------------------------------------------------
+//: \details: TODO
+//: \return:  TODO
+//: \param:   TODO
+//: ----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+        // -------------------------------------------
+        // Get args...
+        // -------------------------------------------
+        char l_opt;
+        std::string l_argument;
+        int l_option_index = 0;
+        struct option l_long_options[] =
+                {
+                { "help",           0, 0, 'h' },
+                { "version",        0, 0, 'V' },
+                // list sentinel
+                { 0, 0, 0, 0 }
+        };
+
+        // -------------------------------------------------
+        // Args...
+        // -------------------------------------------------
+        char l_short_arg_list[] = "hV";
+        while ((l_opt = getopt_long_only(argc, argv, l_short_arg_list, l_long_options, &l_option_index)) != -1)
+        {
+
+                if (optarg)
+                {
+                        l_argument = std::string(optarg);
+                }
+                else
+                {
+                        l_argument.clear();
+                }
+                //printf("arg[%c=%d]: %s\n", l_opt, l_option_index, l_argument.c_str());
+                switch (l_opt)
+                {
+                // ---------------------------------------
+                // Help
+                // ---------------------------------------
+                case 'h':
+                {
+                        print_usage(stdout, 0);
+                        break;
+                }
+                // ---------------------------------------
+                // Version
+                // ---------------------------------------
+                case 'V':
+                {
+                        print_version(stdout, 0);
+                        break;
+                }
+                // What???
+                case '?':
+                {
+                        // Required argument was missing
+                        // '?' is provided when the 3rd arg to getopt_long does not begin with a ':', and is preceeded
+                        // by an automatic error message.
+                        fprintf(stdout, "  Exiting.\n");
+                        print_usage(stdout, -1);
+                        break;
+                }
+                // Huh???
+                default:
+                {
+                        fprintf(stdout, "Unrecognized option.\n");
+                        print_usage(stdout, -1);
+                        break;
+                }
+                }
+        }
+
+        // -------------------------------------------
+        // Do stuff...
+        // -------------------------------------------
+
+        // -------------------------------------------
+        // Cleanup...
+        // -------------------------------------------
+        return 0;
+}
