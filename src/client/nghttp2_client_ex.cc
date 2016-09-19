090cf29a43c6e2bc921dddacc01dc07c8708f8bd
Reed Morrison <rmorrison@edgecast.com>
2016-09-19 00:16:46 -0700

    Colored headersgit diff!

--- src/client/nghttp2_client_ex.cc
+++ src/client/nghttp2_client_ex.cc
@@ -29,6 +29,32 @@
 #include "nghttp2/nghttp2.h"
 
 //: ----------------------------------------------------------------------------
+//: ANSI Color Code Strings
+//:
+//: Taken from:
+//: http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
+//: ----------------------------------------------------------------------------
+#define ANSI_COLOR_OFF          "\033[0m"
+#define ANSI_COLOR_FG_BLACK     "\033[01;30m"
+#define ANSI_COLOR_FG_RED       "\033[01;31m"
+#define ANSI_COLOR_FG_GREEN     "\033[01;32m"
+#define ANSI_COLOR_FG_YELLOW    "\033[01;33m"
+#define ANSI_COLOR_FG_BLUE      "\033[01;34m"
+#define ANSI_COLOR_FG_MAGENTA   "\033[01;35m"
+#define ANSI_COLOR_FG_CYAN      "\033[01;36m"
+#define ANSI_COLOR_FG_WHITE     "\033[01;37m"
+#define ANSI_COLOR_FG_DEFAULT   "\033[01;39m"
+#define ANSI_COLOR_BG_BLACK     "\033[01;40m"
+#define ANSI_COLOR_BG_RED       "\033[01;41m"
+#define ANSI_COLOR_BG_GREEN     "\033[01;42m"
+#define ANSI_COLOR_BG_YELLOW    "\033[01;43m"
+#define ANSI_COLOR_BG_BLUE      "\033[01;44m"
+#define ANSI_COLOR_BG_MAGENTA   "\033[01;45m"
+#define ANSI_COLOR_BG_CYAN      "\033[01;46m"
+#define ANSI_COLOR_BG_WHITE     "\033[01;47m"
+#define ANSI_COLOR_BG_DEFAULT   "\033[01;49m"
+
+//: ----------------------------------------------------------------------------
 //: Macros
 //: ----------------------------------------------------------------------------
 #ifndef _U_
@@ -79,9 +105,13 @@ static void print_header(FILE *f,
                          const uint8_t *value,
                          size_t valuelen)
 {
+        fprintf(f, "%s", ANSI_COLOR_FG_BLUE);
         fwrite(name, namelen, 1, f);
+        fprintf(f, "%s", ANSI_COLOR_OFF);
         fprintf(f, ": ");
+        fprintf(f, "%s", ANSI_COLOR_FG_GREEN);
         fwrite(value, valuelen, 1, f);
+        fprintf(f, "%s", ANSI_COLOR_OFF);
         fprintf(f, "\n");
 }
 
